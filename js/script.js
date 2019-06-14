d265d54a2e082cbf7d7a6afec3fb2d5a081f5fa5
Vincent Renaldo Jita <daejaeblaq@Vincents-MacBook-Pro.local>
2019-06-14 08:17:03 -0700

    done

--- js/script.js
+++ js/script.js
@@ -3,23 +3,24 @@
 //Global Variables
 var sentence = [
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus aperiam, nostrum eos dolorum recusandae repellendus quisquam possimus ex maiores corporis excepturi amet, unde suscipit dolore quo quae voluptas accusantium corrupti.",
-    "Success is most often achieved by those who do not know that failure is inevitable. Things work out best for those who make the best of how things work out. Learn from yesterday, live for today, hope for tomorrow. Don't stop questioning.",
-    "Every one suspects himself of at least one of the cardinal virtues, and this is mine: I am one of the few honest people that I have ever known. Shoot all the bluejays you want, if you can hit them, but remember it’s a sin to kill a mockingbird.",
+    "Success is most often achieved by those who do not know that failure is inevitable. Things work out best for those who make the best of how things work out. Learn from yesterday, live for today, hope for tomorrow. Do not stop questioning.",
+    "Every one suspects himself of at least one of the cardinal virtues, and this is mine: I am one of the few honest people that I have ever known. Shoot all the bluejays you want, if you can hit them, but remember it is a sin to kill a mockingbird.",
     "The Ministry of Peace, which concerned itself with war. The Ministry of Love, which maintained law and order. And the Ministry of Plenty, which was responsible for economic affairs. Their names, in Newspeak: Minitrue, Minipax, Miniluv and Miniplenty.",
     "This paragraph was a waste of time and space. If you had not read this and I had not typed this you and I could have done something more productive than reading this mindlessly and carelessly as if you did not have anything else to do in life.",
     "Life is so precious because it is short and you are being so careless that you do not realize it until now since this void paragraph mentions that you are doing something so mindless, so stupid, so careless that you realize that you are not using your time wisely.",
-    "You want to read this barren paragraph and expect something marvelous and terrific at the end. But since you still do not realize that you are wasting precious time, you still continue to read the null paragraph. Imagine the things you could’ve accomplished."
+    "You want to read this barren paragraph and expect something marvelous and terrific at the end. But since you still do not realize that you are wasting precious time, you still continue to read the null paragraph. Imagine the things you could have accomplished."
 ];
-var gameOver = false;
-const STARTING_TIME = 6;
+const STARTING_TIME = 60;
 var remainingTime = 0;
 var delayHandle = null;
 var timerHandle = null;
 var tempSpan;
 var letterCounter = 0;
-var max = 6;
+var max = 6
 var min = 0; 
 var int; //random number to choose the sentence from sentence array
+var timerCounter;
+var lpm; // letters per minute
 
 //DOM References
 var resetButton;
@@ -30,10 +31,14 @@ var sentenceEl;
 var h2El;
 var pEl;
 var img = "img/nyancat.gif";
+var audioidle;
 var audiostartrace;
-var audio1 = new Audio();
-var audio1 = new Audio();
-var audio1 = new Audio();
+var audioshootingstar;
+var audioyay;
+var audioboo;
+var audiomeow;
+var subdiv;
+// var body;
 
 //Event Listeners
 document.addEventListener("DOMContentLoaded", function() {
@@ -45,32 +50,40 @@ document.addEventListener("DOMContentLoaded", function() {
     sentenceEl = document.querySelector("#sentencebox #highlight");
     pEl = document.getElementById("highlight");
     img = document.getElementById("nyancat");
+    audioidle = document.getElementById("idle");
     audiostartrace = document.getElementById("startrace");
-    // int = Math.floor(Math.random() * (max - min + 1) + min);
-    // console.log(int); //debug
+    audioshootingstar = document.getElementById("shootingstar");
+    audioyay = document.getElementById("yay");
+    audioboo = document.getElementById("boo");
+    audiomeow = document.getElementById("meow");
+    subdiv = document.getElementById("subdiv");
+    // body = document.getElementById("body");
 
-    // makeElementsForSentence(sentence[int]);
-    
-    audiostartrace.play();
+    //play music when mouseover on body
+    // body.addEventListener("mouseover", function() {
+    //     audioidle.play();
+    // });
+
+    //subheading blinking effect
+    setInterval(function() {
+        subdiv.style.visibility = (subdiv.style.visibility == 'hidden' ? '' : 'hidden');
+    }, 1000);
+
+    //generate random number to choose answer key in array sentence
+    int = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(int); //debug
+
+    makeElementsForSentence(sentence[int]);
     initGame();
+    
     
     resetButton.addEventListener("click", function(e) {
         console.log("Clicked reset button!") //debug
         reset();
+        sentenceBox.style.display = 'block';
+        typeBox.focus();
         timerHandle = setInterval(updateClock, 1000);
     });
-    
-    // })
-    
-    //makes span for each letter
-    function makeElementsForSentence(s) {
-        for (let i = 0; i < s.length; i++) {
-            tempSpan = document.createElement('span');
-            tempSpan.id = "l" + i;
-            tempSpan.textContent = s.charAt(i);
-            sentenceBox.appendChild(tempSpan);
-        }
-    }
     
     typeBox.addEventListener("keypress", function(e) {
         // console.log("On keypress event works!") //debug
@@ -86,24 +99,34 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("l" + letterCounter).classList.add("correct");
             letterCounter++;    
             moveCat();
-    }
-    //Check for win
-    if (letterCounter === sentence[int].length) {
-        endGame(true);
-    }
+        }
+        //Check for win
+        if (letterCounter === sentence[int].length) {
+            endGame(true);
+        }
     })
-}
+});
 
 //Additional Functions
 
-function resetAudio() {
-    this.pause();
-    this.currentTime = 0;
-    this.play();
+function makeElementsForSentence(s) {
+    for (let i = 0; i < s.length; i++) {
+        tempSpan = document.createElement('span');
+        tempSpan.id = "l" + i;
+        tempSpan.textContent = s.charAt(i);
+        sentenceBox.appendChild(tempSpan);
+    }
+}
+
+function resetAudio(e) {
+    e.pause();
+    e.currentTime = 0;
+    e.play();
 }
 
 function initGame() {
     letterCounter = 0;
+    sentenceBox.style.display = 'none';
     remainingTime = STARTING_TIME;
     timerText.textContent = remainingTime;
     resetButton.disabled = false;
@@ -116,22 +139,33 @@ function updateClock() {
         endGame(false);
     }
     timerText.textContent = remainingTime;
+    timerCounter++;
+    return timerCounter;
 }
 
 // uses true or false(win) value from checkLetter(), if all sentence are typed correctly, game ends
 function endGame(win) {
     clearTimeout(delayHandle);
     clearInterval(timerHandle);
-    gameOver = true;
     //if resetButton is enabled
     resetButton.disabled = true;
     if(win) { 
         console.log('You won!');
-        h2El.textContent = "Congratulations, you won!"
+        audioyay.play();
+        console.log(letterCounter) //debug
+        console.log(timerCounter) //debug
+        lpm = (letterCounter / timerCounter) * 60;
+        lpm = Math.round(lpm);
+        h2El.textContent = "Congratulations, you won! " + lpm + " letters per minute.";
         initGame();
     } else {
         console.log("You lose, type faster slowpoke!")
-        h2El.textContent = "You LOSE, type faster slowpoke!"
+        audioboo.play();
+        console.log(letterCounter) //debug
+        console.log(timerCounter) //debug
+        lpm = (letterCounter / timerCounter) * 60;
+        lpm = Math.round(lpm);
+        h2El.textContent = "You LOSE, type faster slowpoke! " + lpm + " letters per minute.";
         typeBox.disabled = true;
         delayHandle = setTimeout(function() {
             initGame();
@@ -141,16 +175,25 @@ function endGame(win) {
 
 function moveCat() {
     var i = sentence[int].length / letterCounter
-    console.log(i);
+    console.log(i); //debug
     if (3.9 < i && i < 4.1) {
         img.classList.remove("nyancat1")
         img.classList.add("nyancat2");
+        audiomeow.play();
+        // audiomeow.pause();
+        // audiomeow.currentTime = 0;
     } else if (1.9 < i && i < 2.1) {
         img.classList.remove("nyancat2");
         img.classList.add("nyancat3");
+        audiomeow.play();
+        audiomeow.pause();
+        audiomeow.currentTime = 0;
     } else if (1.2 < i && i < 1.25) {
         img.classList.remove("nyancat3")
         img.classList.add("nyancat4");
+        audiomeow.play();
+        audiomeow.pause();
+        audiomeow.currentTime = 0;
     } else if (i === 1) {
         img.classList.remove("nyancat4")
         img.classList.add("nyancat5");
@@ -158,19 +201,32 @@ function moveCat() {
 }
 
 function reset() {
-    gameOver = false;
+    timerCounter = 0;
     h2El.textContent = "";
     clearTimeout(delayHandle);
     clearInterval(timerHandle);
     sentenceBox.innerHTML = "";
-
+    img.classList.remove("nyancat2");
+    img.classList.remove("nyancat3");
+    img.classList.remove("nyancat4");
+    img.classList.remove("nyancat5");
+    img.classList.add("nyancat1");
+    resetAudio(audiostartrace);
+    resetAudio(audioshootingstar);
+    
     //generate a random number for answer array
     int = Math.floor(Math.random() * (max - min + 1) + min); 
     console.log(int); //debug
     makeElementsForSentence(sentence[int]);
+    
+    //idle audio
+    // audioidle.pause();
+    // audioidle.reset();
+
+    audiostartrace.play();
+    audioshootingstar.play();
 
     initGame();
     typeBox.disabled = false;
-    resetAudio();
+    
 }
-
